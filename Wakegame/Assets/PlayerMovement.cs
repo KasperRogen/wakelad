d3d25f046d02ef20b34119b0c2f575dfedba97a3
Kasper R <kasperhangard@hotmail.com>
2019-07-23 01:13:12 +0200

    PlayerController

--- Wakegame/Assets/PlayerMovement.cs
+++ Wakegame/Assets/PlayerMovement.cs
@@ -22,6 +22,7 @@ public class PlayerMovement : MonoBehaviour
     public float ropeLength;
 
     public LayerMask dampeningMask;
+    public LayerMask groundedMask;
 
     public PlayerStates PlayerState = PlayerStates.RIDING;
     public bool grounded;
@@ -37,10 +38,28 @@ public class PlayerMovement : MonoBehaviour
     {
         Debug.DrawRay(transform.position, Vector3.down * 0.6f, Color.red);
         RaycastHit hit;
-        grounded = Physics.Raycast(transform.position, Vector3.down * 0.5f, out hit, 0.5f, dampeningMask);
+        grounded = Physics.Raycast(transform.position, Vector3.down * 0.5f, out hit, 0.5f, groundedMask);
 
-        return grounded;
+        if(grounded == false)
+        {
+            PlayerState = PlayerStates.AERIAL;
+        }
+        else if(hit.transform.gameObject.layer == LayerMask.NameToLayer("Water"))
+        {
+            PlayerState = PlayerStates.RIDING;
+        }
+        else if (hit.transform.gameObject.layer == LayerMask.NameToLayer("Kicker"))
+        {
+            PlayerState = PlayerStates.KICKER;
+        }
+        else if (hit.transform.gameObject.layer == LayerMask.NameToLayer("Rail"))
+        {
+            PlayerState = PlayerStates.RAIL;
+        } 
+
+        return grounded && hit.transform.gameObject.layer == LayerMask.NameToLayer("Water");
     }
+
 
 
     // Start is called before the first frame update
@@ -59,11 +78,24 @@ public class PlayerMovement : MonoBehaviour
 
     void Steer()
     {
-        moveVector.x = inputVector.x * steerSpeed;
+        switch (PlayerState)
+        {
+            case PlayerStates.RIDING:
+                moveVector.x = inputVector.x * steerSpeed;
+            break;
+
+            case PlayerStates.AERIAL:
+                moveVector.x = rb.velocity.x;
+                transform.RotateAround(transform.position, transform.up, inputVector.x * 10);
+            break;
+
+        }
     }
 
     void Dampen()
     {
+        
+
         if(inputVector.x == 0)
         {
             if(transform.position.x >= 1)
@@ -73,13 +105,13 @@ public class PlayerMovement : MonoBehaviour
             {
                 moveVector.x = 1;
             }
-        } else if (inputVector.x > 0)
+        } else if (inputVector.x > 0 && PlayerState == PlayerStates.RIDING)
         {
             if(transform.position.x >= ropeLength * 0.75)
             {
                 moveVector.x += (ropeLength * 0.75f) - transform.position.x;
             }
-        } else if (inputVector.x < 0)
+        } else if (inputVector.x < 0 && PlayerState == PlayerStates.RIDING)
         {
             if (transform.position.x <= -ropeLength * 0.75)
             {
@@ -96,27 +128,27 @@ public class PlayerMovement : MonoBehaviour
         inputVector.x = Input.GetAxis("Horizontal");
         inputVector.z = Input.GetAxis("Vertical");
 
-        PlayerState = IsGrounded() ? PlayerStates.RIDING : PlayerStates.AERIAL;
+        if (moveVector == Vector3.zero)
+            moveVector = rb.velocity;
+        
 
+        
+        IsGrounded();
         Move();
         Steer();
         Dampen();
 
-        switch (PlayerState)
-        {
-            case PlayerStates.RIDING:
-                transform.LookAt(transform.position + new Vector3(rb.velocity.x, 0, rb.velocity.z));
-            break;
-
-            case PlayerStates.AERIAL:
-                transform.LookAt(transform.position + new Vector3(rb.velocity.x, 0, rb.velocity.z));
-            break;
 
 
-        }
+        transform.LookAt(transform.position + new Vector3(rb.velocity.x, 0, rb.velocity.z));
 
+
+        if(moveVector != Vector3.zero)
         rb.velocity = moveVector;
+
         Debug.DrawRay(transform.position, moveVector, Color.black, 20);
+
+        moveVector = Vector3.zero;
     }
 
 
@@ -132,7 +164,7 @@ public class PlayerMovement : MonoBehaviour
         if(collision.gameObject.tag == "Kicker")
         {
             Debug.DrawRay(transform.position, rb.velocity, Color.cyan, 20);
-            Vector3 velocity = moveVector;
+            Vector3 velocity = rb.velocity;
             Quaternion oldRot = transform.rotation;
 
             ContactPoint hit = collision.contacts[0];
